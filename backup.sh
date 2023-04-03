b486c2bf7f1cf6f1776bccf0aed80627ff34d0d1
Владислав Барсуков <vladislavbarsukov@Vladislavs-MacBook-Air.local>
2023-04-03 23:44:38 +0500

    snapshot

--- backup.sh
+++ backup.sh
@@ -1,3 +1,8 @@
 ENVSETTINGSREPO=~/Src/EnvSettings
 
 cp -f $HOME/Library/Application\ Support/Code/User/settings.json $ENVSETTINGSREPO/shared/settings.json
+
+cd $ENVSETTINGSREPO 
+git add .
+git commit -m "snapshot"
+git push origin
