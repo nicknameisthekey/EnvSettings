c8bd7c6bb80008197c83f3c48c4d9d9d4a74c144
Владислав Барсуков <vladislavbarsukov@Vladislavs-MacBook-Air.local>
2023-04-04 00:51:26 +0500

    snapshot

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@ ENVSETTINGSREPO=~/src/EnvSettings
 
 cp -f $HOME/Library/Application\ Support/Code/User/settings.json $ENVSETTINGSREPO/shared/settings.json
 cp -f $HOME/.zshrc $ENVSETTINGSREPO/macos/.zshrc
+cp -f $HOME/Library/Preferences/com.googlecode.iterm2.plist $ENVSETTINGSREPO/macos/iterm2.plist
 
 cd $ENVSETTINGSREPO 
 git add .
