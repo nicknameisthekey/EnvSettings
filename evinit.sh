57bee26186fbdc916b5cc1ebfb0b85cea42f2312
Владислав Барсуков <vladislavbarsukov@Vladislavs-MacBook-Air.local>
2023-04-14 20:13:42 +0500

    snapshot

--- evinit.sh
+++ evinit.sh
@@ -9,3 +9,7 @@ brew install --cask iterm2
 brew install oh-my-posh
 brew install docker
 brew install lazydocker
+brew install vlc --cask
+brew install lsd
+mkdir ~/.config/lsd
+cp ~/src/ENVSETTINGS/lsd.yaml ~/.config/lsd/config.yaml
--- macos/.zshrc
+++ macos/.zshrc
@@ -1,2 +1,3 @@
 eval "$(oh-my-posh init zsh --config ~/src/EnvSettings/shared/theme.omp.json)"
 alias lzd=lazydocker
+alias ls=lsd
