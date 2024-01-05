e69412f58d0dbf73ab549933d673b42c3f3e7dbd
Nicknameisthekey <Nicknameisthekey@gmail.com>
2024-01-05 11:07:45 +0900

    snapshot

--- backup.sh
+++ backup.sh
@@ -3,7 +3,9 @@ ENVSETTINGSREPO=~/src/EnvSettings
 cp -f $HOME/Library/Application\ Support/Code/User/settings.json $ENVSETTINGSREPO/shared/settings.json
 cp -f $HOME/.zshrc $ENVSETTINGSREPO/macos/.zshrc
 cp -f $HOME/Library/Preferences/com.googlecode.iterm2.plist $ENVSETTINGSREPO/macos/iterm2.plist
-
+cp -f /Users/vladislavbarsukov/.config/helix/languages.toml $ENVSETTINGSREPO/macos/helix/languages.toml
+cp -f /Users/vladislavbarsukov/.config/helix/config.toml $ENVSETTINGSREPO/macos/helix/config.toml
+cp -f /Users/vladislavbarsukov/.config/lsd/config.yaml $ENVSETTINGSREPO/macos/lsd/config.yaml
 cd $ENVSETTINGSREPO 
 git add .
 git commit -m "snapshot"
--- macos/.zshrc
+++ macos/.zshrc
@@ -1,6 +1,10 @@
 eval "$(oh-my-posh init zsh --config ~/src/EnvSettings/shared/theme.omp.json)"
 alias lzd=lazydocker
+alias lzg=lazygit
 alias ls='lsd -a'
-alias lsa='lsa -la'
+alias lsa='lsd -la'
+alias lst='lsd --tree'
 alias books='code /Users/vladislavbarsukov/src/BookNotes'
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm@16/bin:$PATH"
+export PATH="/Users/vladislavbarsukov/.vscode/extensions/ms-dotnettools.csharp-1.26.0-darwin-arm64/.omnisharp/1.39.7-net6.0:$PATH"
