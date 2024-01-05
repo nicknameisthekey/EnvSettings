174fdecc94859393652c6f76e0f895bebee1981d
Nicknameisthekey <Nicknameisthekey@gmail.com>
2024-01-05 21:23:54 +0900

    snapshot

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ cp -f $HOME/Library/Preferences/com.googlecode.iterm2.plist $ENVSETTINGSREPO/mac
 cp -f /Users/vladislavbarsukov/.config/helix/languages.toml $ENVSETTINGSREPO/macos/helix/languages.toml
 cp -f /Users/vladislavbarsukov/.config/helix/config.toml $ENVSETTINGSREPO/macos/helix/config.toml
 cp -f /Users/vladislavbarsukov/.config/lsd/config.yaml $ENVSETTINGSREPO/macos/lsd/config.yaml
+cp -f /Users/vladislavbarsukov/.zshrc $ENVSETTINGSREPO/macos/.zshrc
 cd $ENVSETTINGSREPO 
 git add .
 git commit -m "snapshot"
--- macos/.zshrc
+++ macos/.zshrc
@@ -4,7 +4,6 @@ alias lzg=lazygit
 alias ls='lsd -a'
 alias lsa='lsd -la'
 alias lst='lsd --tree'
-alias books='code /Users/vladislavbarsukov/src/BookNotes'
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/opt/llvm@16/bin:$PATH"
 export PATH="/Users/vladislavbarsukov/.vscode/extensions/ms-dotnettools.csharp-1.26.0-darwin-arm64/.omnisharp/1.39.7-net6.0:$PATH"
